17cbec1d483d1bd03816a80960d3a7932a353ace
Deniz Lucas <deniz4376@gmail.com>
2025-09-06 06:25:52 +0300

    I changed the loop initialization i to 0 instead of starting at 1

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,19 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger
     let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+    debugger
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
+    debugger
 
     return totalPrice;
 }
 
-
+calculateDiscountedPrice(10, 5);
 
 module.exports = calculateDiscountedPrice;
